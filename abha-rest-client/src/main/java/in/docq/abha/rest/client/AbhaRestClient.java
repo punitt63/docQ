511f9fd3bf9127ba0417969ddfb70fa77efc64c2
ppichhol <punit.pichholia@oracle.com>
2025-05-30 14:58:53 +0530

    abha rest client common code changes

--- abha-rest-client/src/main/java/in/docq/abha/rest/client/AbhaRestClient.java
+++ abha-rest-client/src/main/java/in/docq/abha/rest/client/AbhaRestClient.java
@@ -5,15 +5,14 @@ import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
-import in.docq.abha.rest.client.api.GatewaySessionApi;
-import in.docq.abha.rest.client.api.HealthFacilitySearchApi;
-import in.docq.abha.rest.client.api.HealthProfessionalSearchApi;
-import in.docq.abha.rest.client.model.ApiHiecmGatewayV3SessionsPostRequest;
-import in.docq.abha.rest.client.model.SearchByHprIdRequest;
-import in.docq.abha.rest.client.model.SearchFacilitiesData;
-import in.docq.abha.rest.client.model.SearchForFacilitiesRequest;
+import in.docq.abha.rest.client.api.*;
+import in.docq.abha.rest.client.model.*;
 
 import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletionStage;
 
@@ -29,6 +28,12 @@ public class AbhaRestClient {
     private static final String refreshTokenGrantType = "refresh_token";
     private static final String accessTokenCacheKey = "clientAccessToken";
     private static final String refreshTokenCacheKey = "clientRefreshToken";
+    private final AbhaLoginApi abhaLoginApi;
+    private final AbhaCardApi abhaCardApi;
+    private final AbhaEnrollmentViaAadhaarApi abhaEnrollmentViaAadhaarApi;
+    private final AbhaAddressVerificationApi abhaAddressVerificationApi;
+    private final AbhaProfileApi abhaProfileApi;
+
     private final HealthFacilitySearchApi healthFacilitySearchApi;
     private final HealthProfessionalSearchApi healthProfessionalSearchApi;
     private final Cache<String, String> tokenCache;
@@ -36,6 +41,15 @@ public class AbhaRestClient {
 
     public AbhaRestClient(ApiClient apiClient, String clientId, String clientSecret) {
         this.apiClient = apiClient;
+        this.abhaLoginApi = new AbhaLoginApi(apiClient);
+        this.abhaLoginApi.setCustomBaseUrl("https://abhasbx.abdm.gov.in");
+        this.abhaCardApi = new AbhaCardApi(apiClient);
+        this.abhaEnrollmentViaAadhaarApi = new AbhaEnrollmentViaAadhaarApi(apiClient);
+        this.abhaEnrollmentViaAadhaarApi.setCustomBaseUrl("https://abhasbx.abdm.gov.in");
+        this.abhaAddressVerificationApi = new AbhaAddressVerificationApi(apiClient);
+        this.abhaAddressVerificationApi.setCustomBaseUrl("https://abhasbx.abdm.gov.in");
+        this.abhaProfileApi = new AbhaProfileApi(apiClient);
+        this.abhaProfileApi.setCustomBaseUrl("https://abhasbx.abdm.gov.in");
         this.healthFacilitySearchApi = new HealthFacilitySearchApi(apiClient);
         this.healthProfessionalSearchApi = new HealthProfessionalSearchApi(apiClient);
         this.gatewaySessionApi = new GatewaySessionApi(apiClient);
@@ -80,7 +94,7 @@ public class AbhaRestClient {
                 new ApiHiecmGatewayV3SessionsPostRequest()
                         .clientId(clientId)
                         .clientSecret(clientSecret)
-                        .grantType(refreshTokenGrantType);
+                        .grantType(accessTokenGrantType);
         return gatewaySessionApi.apiHiecmGatewayV3SessionsPostAsync(Instant.now().toString(), UUID.randomUUID().toString(), "sbx", apiHiecmGatewayV3SessionsPostRequest)
                 .thenAccept(response -> {
                     tokenCache.put(accessTokenCacheKey, response.getAccessToken());
@@ -96,7 +110,7 @@ public class AbhaRestClient {
 
     private String getCachedAccessToken() {
         String accessToken = tokenCache.getIfPresent(accessTokenCacheKey);
-        checkState(accessToken != null, "access token in cache is null");
+        //checkState(accessToken != null, "access token in cache is null");
         return accessToken;
     }
 
@@ -132,5 +146,185 @@ public class AbhaRestClient {
                         throw new ApiException(404, "Health Professional ID " + healthProfessionalID + " Doesn't Exist");
                     }
                 });
+    }
+
+    public CompletionStage<AbhaApiV3EnrollmentRequestOtpPost200Response> abhaEnrollmentRequestOtp(String encryptedAadharNumber) {
+        return getAccessToken()
+                .thenCompose(token -> abhaEnrollmentViaAadhaarApi.abhaApiV3EnrollmentRequestOtpPostAsync(token, UUID.randomUUID().toString(), Instant.now().toString(),
+                        new AbhaApiV3EnrollmentRequestOtpPostRequest()
+                                .scope(List.of("abha-enrol"))
+                                .loginHint("aadhaar")
+                                .loginId(encryptedAadharNumber)
+                                .otpSystem("aadhaar")));
+    }
+
+    public CompletionStage<AbhaApiV3EnrollmentEnrolByAadhaarPost200Response> abhaEnrollmentByAadhar(List<String> authMethods, String txnId, String otpValue, String mobile) {
+        AbhaApiV3EnrollmentEnrolByAadhaarPostRequestAuthData abhaApiV3EnrollmentEnrolByAadhaarPostRequestAuthData = new AbhaApiV3EnrollmentEnrolByAadhaarPostRequestAuthData();
+        abhaApiV3EnrollmentEnrolByAadhaarPostRequestAuthData.setActualInstance(new AbhaApiV3EnrollmentEnrolByAadhaarPostRequestAuthDataAnyOf()
+                .otp(new AbhaApiV3EnrollmentEnrolByAadhaarPostRequestAuthDataAnyOfOtp()
+                        .otpValue(otpValue)
+                        .mobile(mobile)
+                        .txnId(txnId)));
+        return getAccessToken().thenCompose(token -> {
+            try {
+                return abhaEnrollmentViaAadhaarApi.abhaApiV3EnrollmentEnrolByAadhaarPostAsyncCall(Instant.now().toString(), UUID.randomUUID().toString(), null, token,
+                        new AbhaApiV3EnrollmentEnrolByAadhaarPostRequest()
+                                .authData(abhaApiV3EnrollmentEnrolByAadhaarPostRequestAuthData)
+                                .consent(new AbhaApiV3EnrollmentEnrolByAadhaarPostRequestConsent().code("abha-enrollment").version("1.4")));
+            } catch (ApiException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public CompletionStage<AbhaApiV3EnrollmentEnrolSuggestionGet200Response> getAbhaAddressSuggestions(String txnId) {
+        String timestamp = ZonedDateTime.now(ZoneOffset.UTC)
+                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+
+        return getAccessToken()
+                .thenCompose(token -> abhaEnrollmentViaAadhaarApi.abhaApiV3EnrollmentEnrolSuggestionGetAsyncCall(token, txnId, UUID.randomUUID().toString(), timestamp));
+    }
+
+    public CompletionStage<AbhaApiV3EnrollmentEnrolAbhaAddressPost200Response> enrolAbhaAddress(String txnId, String abhaAddress, String preferred) {
+        return getAccessToken().thenCompose(token -> {
+            try {
+                return abhaEnrollmentViaAadhaarApi.abhaApiV3EnrollmentEnrolAbhaAddressPostAsyncCall(token, UUID.randomUUID().toString(), Instant.now().toString(),
+                        new AbhaApiV3EnrollmentEnrolAbhaAddressPostRequest()
+                                .txnId(txnId)
+                                .abhaAddress(abhaAddress)
+                                .preferred(preferred));
+            } catch (ApiException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public CompletionStage<AbhaApiV3ProfileLoginRequestOtpPost200Response> abhaLoginRequestOtp(List<String> scopes, String loginHint, String loginId, String otpSystem) {
+        return getAccessToken()
+                .thenCompose(token -> {
+                    try {
+                        return abhaLoginApi.abhaApiV3ProfileLoginRequestOtpPostAsyncCall(token, UUID.randomUUID().toString(), Instant.now().toString(),
+                                new AbhaApiV3ProfileLoginRequestOtpPostRequest()
+                                        .scope(scopes)
+                                        .loginHint(loginHint)
+                                        .loginId(loginId)
+                                        .otpSystem(otpSystem));
+                    } catch (ApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    public CompletionStage<AbhaApiV3ProfileLoginVerifyPost200Response> abhaLoginVerifyOtp(List<String> scopes, List<String> authMethods, String txnId, String otp) {
+        return getAccessToken()
+                .thenCompose(token -> {
+                    try {
+                        return abhaLoginApi.abhaApiV3ProfileLoginVerifyPostAsyncCall(token, UUID.randomUUID().toString(), Instant.now().toString(),
+                                new AbhaApiV3ProfileLoginVerifyPostRequest()
+                                        .scope(scopes)
+                                        .authData(new AbhaApiV3ProfileLoginVerifyPostRequestAuthData()
+                                                .authMethods(authMethods)
+                                                .otp(new AbhaApiV3ProfileLoginVerifyPostRequestAuthDataOtp()
+                                                        .otpValue(otp)
+                                                        .txnId(txnId))));
+                    } catch (ApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    public CompletionStage<AbhaApiV3ProfileLoginVerifyUserPost200Response> abhaLoginVerifyUser(String abhaNumber, String txnId, String tToken) {
+        return getAccessToken()
+                .thenCompose(token -> {
+                    try {
+                        return abhaLoginApi.abhaApiV3ProfileLoginVerifyUserPostAsyncCall(token, tToken, UUID.randomUUID().toString(), Instant.now().toString(),
+                                new AbhaApiV3ProfileLoginVerifyUserPostRequest()
+                                        .abHANumber(abhaNumber)
+                                        .txnId(txnId));
+                    } catch (ApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    public CompletionStage<AbhaApiV3PhrWebLoginAbhaSearchPost200Response> searchAbhaAddress(String abhaAddress) {
+        return getAccessToken()
+                .thenCompose(token -> {
+                    try {
+                        return abhaAddressVerificationApi.abhaApiV3PhrWebLoginAbhaSearchPostAsyncCall(token, UUID.randomUUID().toString(), Instant.now().toString(),
+                                new AbhaApiV3PhrWebLoginAbhaSearchPostRequest()
+                                        .abhaAddress(abhaAddress));
+                    } catch (ApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    public CompletionStage<AbhaApiV3PhrWebLoginAbhaRequestOtpPost200Response> abhaAddressLoginRequestOtp(List<String> scopes, String loginHint, String loginId, String otpSystem) {
+        return getAccessToken()
+                .thenCompose(token -> {
+                    try {
+                        return abhaAddressVerificationApi.abhaApiV3PhrWebLoginAbhaRequestOtpPostAsyncCall(token, UUID.randomUUID().toString(), Instant.now().toString(),
+                                new AbhaApiV3PhrWebLoginAbhaRequestOtpPostRequest()
+                                        .scope(scopes)
+                                        .loginHint(loginHint)
+                                        .loginId(loginId)
+                                        .otpSystem(otpSystem));
+                    } catch (ApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    public CompletionStage<AbhaApiV3PhrWebLoginAbhaVerifyPost200Response> abhaAddressLoginVerifyOtp(List<String> scopes, List<String> authMethods, String txnId, String otp) {
+        AbhaApiV3PhrWebLoginAbhaVerifyPostRequestAuthData abhaApiV3PhrWebLoginAbhaVerifyPostRequestAuthData = new AbhaApiV3PhrWebLoginAbhaVerifyPostRequestAuthData();
+        abhaApiV3PhrWebLoginAbhaVerifyPostRequestAuthData.setActualInstance(new AbhaApiV3PhrWebLoginAbhaVerifyPostRequestAuthDataAnyOfInner()
+                .otp(new AbhaApiV3PhrWebLoginAbhaVerifyPostRequestAuthDataAnyOfInnerOtp()
+                        .otpValue(otp)
+                        .txnId(txnId)));
+        return getAccessToken()
+                .thenCompose(token -> {
+                    try {
+                        return abhaAddressVerificationApi.abhaApiV3PhrWebLoginAbhaVerifyPostAsyncCall(token, UUID.randomUUID().toString(), Instant.now().toString(),
+                                new AbhaApiV3PhrWebLoginAbhaVerifyPostRequest()
+                                        .scope(scopes)
+                                        .authData(abhaApiV3PhrWebLoginAbhaVerifyPostRequestAuthData));
+                    } catch (ApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    public CompletionStage<AbhaApiV3PhrWebLoginProfileAbhaProfileGet200Response> getAbhaAddressProfile(String xToken) {
+        return getAccessToken()
+                .thenCompose(token -> abhaAddressVerificationApi.abhaApiV3PhrWebLoginProfileAbhaProfileGetAsyncCall(token, xToken, UUID.randomUUID().toString(), Instant.now().toString()));
+    }
+
+    public CompletionStage<String> getAbhaCard(String xToken) {
+        return getAccessToken()
+                .thenCompose(token -> abhaAddressVerificationApi.abhaApiV3PhrWebLoginProfileAbhaPhrCardGetAsyncCall(token, xToken, UUID.randomUUID().toString(), Instant.now().toString()));
+    }
+
+    public CompletionStage<AbhaApiV3ProfileAccountRequestOtpPost200Response> abhaDeletionRequestOtp(String xToken, List<String> scopes, String loginHint, String loginId, String otpSystem) {
+        return getAccessToken()
+                .thenCompose(token -> abhaProfileApi.abhaApiV3ProfileAccountRequestOtpPostAsyncCall(token, xToken, UUID.randomUUID().toString(), Instant.now().toString(),
+                        new AbhaApiV3ProfileAccountRequestOtpPostRequest()
+                                .scope(scopes)
+                                .loginHint(loginHint)
+                                .loginId(loginId)
+                                .otpSystem(otpSystem)));
+    }
+
+    public CompletionStage<AbhaApiV3ProfileAccountVerifyPost200Response> abhaDeletionVerifyOtp(String xToken, List<String> scopes, List<String> authMethods, String txnId, String otp, List<String> reasons) {
+        return getAccessToken()
+                .thenCompose(token -> abhaProfileApi.abhaApiV3ProfileAccountVerifyPostAsyncCall(token, xToken, UUID.randomUUID().toString(), Instant.now().toString(),
+                        new AbhaApiV3ProfileAccountVerifyPostRequest()
+                                .scope(scopes)
+                                .authData(new AbhaApiV3ProfileAccountVerifyPostRequestAuthData()
+                                        .authMethods(authMethods)
+                                        .otp(new AbhaApiV3ProfileAccountVerifyPostRequestAuthDataOtp()
+                                                .otpValue(otp)
+                                                .txnId(txnId)))
+                                .reasons(reasons)));
     }
 }
